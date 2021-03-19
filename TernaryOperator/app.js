396a414bb124eb8f6e11ba6167c5ea4d9aa32fbf
ugurbatikan <batikanugur@hotmail.com>
2021-03-19 18:12:49 +0300

    TernaryOperator

---
+++ TernaryOperator/app.js
@@ -0,0 +1,10 @@
+// condition ? expIfTrue : expIfFalse
+
+let number = 1;
+number ===7 ? console.log('LUCKY!') : console.log('BAD!!');
+
+let status = 'offline';
+let color = status === 'offline' ? 'red' : 'green';
+
+let score = 1200;
+let game = score >= 1300 ? 'WIN!!' : 'TRY AGAIN!!';
