9439eed467be0d52e3537ef1399fe53bbf0e56b1
ugurbatikan <batikanugur@hotmail.com>
2021-03-19 18:28:20 +0300

    RunningCodeFromFile

---
+++ 04RunningCodeFromFile/app.js
@@ -0,0 +1,9 @@
+alert('Hello From JS!');
+//it wont show
+"hi".toUpperCase();
+console.log("hi".toUpperCase());
+//it will show
+console.log("hi".toUpperCase());
+console.log('3==3:' , 3 =='3');
+console.log('3===3:', 3==='3');
+console.error("OHH ERROR!!");
