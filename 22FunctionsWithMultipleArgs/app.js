a894db95d2d650810a77b76c7b853f012cdfe524
ugurbatikan <batikanugur@hotmail.com>
2021-03-20 01:29:38 +0300

    FunctionsWithMultipleArgs

---
+++ 22FunctionsWithMultipleArgs/app.js
@@ -0,0 +1,15 @@
+
+function square(num){
+    console.log(num * num);
+}
+square(5);
+
+function sum(x, y){
+    console.log(`${x} + ${y} = ` , x + y);
+}
+sum(12,15);
+
+function divide(a, b){
+    console.log(`${a} / ${b} = `, a /b);
+}
+divide(10, 3);
