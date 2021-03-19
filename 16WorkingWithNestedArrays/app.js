f53630e22d02dd5dafbd87b1b6653e1774975226
ugurbatikan <batikanugur@hotmail.com>
2021-03-19 18:35:23 +0300

    Nested Arrays

---
+++ 16WorkingWithNestedArrays/app.js
@@ -0,0 +1,8 @@
+//Nested Arrays
+//We can store arrays inside other arrays
+const colors = [
+    ['red','crimson'],['orange','dark orange'], ['yellow','dark Yellow'], ['green','olive']
+]
+colors[2] = ['Pink','Purple'];
+console.log("Colors:"+colors[3][1]);
+
