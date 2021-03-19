f940b137146dd57da9f6b52fee36b3f9bbc22760
ugurbatikan <batikanugur@hotmail.com>
2021-03-20 01:30:12 +0300

    TheReturnStatement

---
+++ 23TheReturnStatement/app.js
@@ -0,0 +1,22 @@
+/* // RETURN
+Builts-in methods return values when we call them
+we can store those values */
+ //NO RETURN 
+ //Our functions print values out, but do NOT  return anything
+ 
+ /* function add (x, y){
+     console.log(x + y);
+ }
+ add(2,3); // 2 + 3 = 5
+ const sum = add(10, 6); // 16
+ sum; // Undefined */
+
+ // RETURN
+  
+ function add(x, y){
+    return x + y;
+ }
+ const sum = add (10, 6);
+ sum; // 16
+ const answer = add (100,150);
+ answer;
