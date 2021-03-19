f618e6eece67cb4f599c2ebe1fccd8aebca46fe1
ugurbatikan <batikanugur@hotmail.com>
2021-03-19 18:27:12 +0300

    delete

--- 04RunningCodeFromFile/app.js
+++
@@ -1,9 +0,0 @@
-alert('Hello From JS!');
-//it wont show
-"hi".toUpperCase();
-console.log("hi".toUpperCase());
-//it will show
-console.log("hi".toUpperCase());
-console.log('3==3:' , 3 =='3');
-console.log('3===3:', 3==='3');
-console.error("OHH ERROR!!");
