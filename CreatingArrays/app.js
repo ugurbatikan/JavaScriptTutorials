d013299294bbddb9beca96db7fdc24a3727340d0
ugurbatikan <batikanugur@hotmail.com>
2021-03-19 17:56:37 +0300

    Arrays & Methods

---
+++ CreatingArrays/app.js
@@ -0,0 +1,33 @@
+// make an empty Array
+let student = [];
+// An Array of String
+let colors = ['red', 'orange','blue']; 
+// An Array of Numbers
+let numbers = [1,2,3,4,5,6];
+// A mixed Array
+let stuff = [true, 68, 'cat', null];
+
+// Arrays are Indexed
+
+let color = ['red','green','blue', 'orange','yellow','purple'];
+color.length; // 6
+color[0]; // red
+color[1]; // green
+color[2]; // blue
+color[3]; // orange
+color[4]; // yellow
+color[5]; // purple
+
+// Modifiyng Arrays
+
+let animals = ['dog', 'chicken' , 'cat', 'shark', 'elepasda'];
+animals[0];
+animals [1] = 'worm';
+animals [3] = 'dolphin';
+animals [animals.length-1] =  'elephant';
+  // ['dog , worm, cat, dolphin, elephant]
+  let haha = ['hahaha'];
+  haha[0];
+
+
+
