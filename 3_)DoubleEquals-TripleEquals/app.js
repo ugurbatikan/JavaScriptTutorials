cd97502b73e13cdef038d8860821ccab5641e0d9
ugurbatikan <batikanugur@hotmail.com>
2021-03-19 17:18:06 +0300

    Double Equals & Triple Equals

---
+++ 3_)DoubleEquals-TripleEquals/app.js
@@ -0,0 +1,24 @@
+// == DOUBLE EQUALS ==
+
+5 == 5; // true
+'b' == 'c'; //false
+7 == '7'; // true
+0 == ''; // true
+true == false; //false
+0 == false; // true
+null == undefined; // true
+
+
+//=== TRIPLE EQUALS ===
+
+5 === 5; //true
+'b' === 'c'; // false
+7 ==='7'; //false
+0 === ''; // false
+true === false; // false
+0 === false; // false
+null === undefined; // false
+
+//!= and !==
+10 != '10'; //false
+10 !== '10'; // true
