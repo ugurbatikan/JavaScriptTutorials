c23123f7c98e144e3158dab4efe60bd0298c7f32
ugurbatikan <batikanugur@hotmail.com>
2021-03-19 18:29:09 +0300

    Conditionals

---
+++ 05Conditionals/app.js
@@ -0,0 +1,28 @@
+//IF STATEMENT
+
+// if(1 == 1){
+//     console.log('Its True');
+
+// }
+// Example 1
+let rating = 3;
+if(rating === 3){
+    console.log("YOU ARE A SUPER STAR!");
+}
+
+// Example 2
+let evens = 17;
+if(evens % 2 !== 0){
+    console.log("AWESOME ITS AN ODD NUMBER!");
+}
+
+// ELSE IF STATEMENT
+
+let rating2 = 2;
+if(rating2 === 3){
+    console.log("YOU ARE SUPERSTAR!");
+}
+else if(rating2 === 2){
+    console.log("MEETS EXPECTETIONS!");
+
+}
