edb5213e1802f19a8c586f055d1d882264ba47fc
ugurbatikan <batikanugur@hotmail.com>
2021-03-19 18:38:07 +0300

    Functions

---
+++ 19Functions/app.js
@@ -0,0 +1,26 @@
+/* Write valid functions
+Write functions with arguments
+Compare function expressions & function statements
+Write a bunch of functions */
+
+// Reusable procedures
+// We can use them ALL THE TIME!
+// 2 step process -->  define than run it!
+//  DEFINE
+// function funcName(){
+    // do something
+//}
+//RUN
+/* funcName(); // run once
+funcName(); // run again */
+function grumpus(){
+    console.log('Hello..');
+    console.log('ughh ... ggzz..');
+    console.log('For the last time');
+    console.log('Leave me alone!!..');
+}
+grumpus();
+
+for(let i = 0; i <= 5; i++){
+    grumpus();
+}
