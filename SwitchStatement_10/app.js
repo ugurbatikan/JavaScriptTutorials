54ab1ddacd0641725654e788e8081b7d200fac8c
ugurbatikan <batikanugur@hotmail.com>
2021-03-19 17:35:48 +0300

    SwitchStatement

---
+++ SwitchStatement_10/app.js
@@ -0,0 +1,51 @@
+let day = 2;
+switch (day) {
+    case 1:
+        console.log('MONDAY');
+        break;
+        case 2:
+        console.log('TUESDAY');
+        break;
+        case 3:
+        console.log('WEDNESDAY');
+        break;
+        case 4:
+        console.log('THURSDAY');
+        break;
+        case 5:
+        console.log('FRIDAY');
+        break;
+        case 6:
+        console.log('SATURDAY');
+        break;
+        case 7:
+        console.log('SUNDAY');
+        break;
+
+    default:
+        console.log('INVALID DAY!');
+       
+}
+ let emoji = 'plants';
+switch (emoji) {
+    case 'sad face':
+        case 'happy face':
+            console.log('yellow');
+        
+        break;
+        case 'heart':
+            case 'lips':
+                console.log('red');
+                break;
+                case 'eggplant':
+                    console.log('purple');
+                    break;
+                    case 'frog':
+                        case 'plants':
+                            case 'sick face':
+                                console.log('green');
+
+
+    default:
+        break;
+}
