0dc1a1d2f1ee952ec72e232a60403a608f9e00ea
ugurbatikan <batikanugur@hotmail.com>
2021-03-19 17:58:38 +0300

    IntroToReferanceTypes

---
+++ IntroToReferanceTypes/app.js
@@ -0,0 +1,17 @@
+let fruits = 'orange';
+let color = fruits;
+
+console.log(fruits);
+fruits = 'apple';
+console.log(color);
+console.log(fruits);
+
+
+let arrFruits = ['elma','nar','erik'];
+let arrRengiKirmizi = arrFruits;
+console.log(arrFruits);
+console.log(arrRengiKirmizi);
+arrFruits.push('Muz');
+console.log("Fruits:"+ arrFruits);
+
+console.log("Kirmizi Renk:"+ arrRengiKirmizi);
