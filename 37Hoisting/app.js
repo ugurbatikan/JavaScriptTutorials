d1e90943b244460b28684c6b57b383f1f3dcfbdf
ugurbatikan <batikanugur@hotmail.com>
2021-04-05 17:50:46 +0300

    Hoisting

---
+++ 37Hoisting/app.js
@@ -0,0 +1,25 @@
+/* //Hoisting example
+console.log(animal); // undefined
+var animal = 'elephant';
+ 
+// Reference Error
+console.log(mamals);
+let mamals = 'dog'; */
+
+ /* // Reference Error
+console.log(play);
+const play = 'play with me'; */
+
+
+/* cowSound(); // MMÖÖÖÖÖÖĞĞ
+function cowSound(){
+  console.log('MMÖÖÖÖÖÖĞĞ');
+}
+cowSound(); // MMÖÖÖÖÖÖĞĞ */
+
+//console.log(hello); // undefined
+//hello(); // is not a function hoisting
+var hello = function(){
+  console.log('helloo');
+}
+
