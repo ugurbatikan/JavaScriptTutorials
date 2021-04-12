0fb3d441481ec0218579c94f724009411469394a
ugurbatikan <batikanugur@hotmail.com>
2021-04-12 21:28:15 +0300

    Arrow Functions

---
+++ 41ArrowfunctionsIntro/app.js
@@ -0,0 +1,27 @@
+//Arrow functios is not supported IE
+/* const square = function (x) {
+  return x * x;
+} */
+//Examples
+/* const square = (x) => {
+  return x * x;
+} */
+//if we have 1 parameter we dont need parenthesis
+const square = x => {
+  return x * x;
+}
+
+const sum = (x, y) =>{
+  return x + y;
+}
+/* const isEven = (num) =>{
+  return num % 2 === 0;
+} */
+const isEven = num => {
+  return num % 2 === 0;
+}
+const greet = () => {
+  console.log('HI');
+}
+
+
